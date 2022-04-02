a59139e1f1917ca3229cb2af05594570a2cdd570
Jakub Pilarek <pilarek.jakub@gmail.com>
2022-04-02 12:24:37 +0200

    Drugi wpis w historii plików

---
+++ src/NowaKlasa.java
@@ -0,0 +1,18 @@
+/*
+ komentarz
+ wieloliniowy
+ */
+
+/**
+ * ta klasa jest moja
+ *
+ * ten komentarz jest czescia dokumentacji!
+ */
+public class NowaKlasa {
+    // Do zrobienia
+    //  TODO: musze pamietac zeby pocwiczyc
+    // fixme: kup mleko
+
+    // Kod powinien sie sam dokumentować
+
+   }
